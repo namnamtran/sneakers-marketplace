8af85b774478bbb78ddc2ad5749be7a04df1e183
namnamtran <122743898+namnamtran@users.noreply.github.com>
2024-05-08 15:40:20 -0400

    Setting Up Backend

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ErrorHandler = require('./utils/errorHandler');
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload({useTempFiles: true}));
+
+
+// config 
+if(process.env.NODE_ENV !== 'PRODUCTION'){
+    require("dotenv").config({
+        path:"config/.env"
+    })
+}
+
+app.use(ErrorHandler);
+
+module.exports = app;
